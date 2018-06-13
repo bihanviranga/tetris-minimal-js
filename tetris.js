1e6f3195b60350d46ced9650804caf6eec4b2770
Bihan Viranga <bihanviranga@gmail.com>
2018-06-13 15:33:39 +0530

    Single tile drops. Left/right/down works.

---
+++ tetris.js
@@ -0,0 +1,76 @@
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+
+context.scale(20,20);
+
+
+const matrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+];
+
+
+
+function drawMatrix(matrix,offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = 'red';
+                context.fillRect(x+offset.x,
+                                 y+offset.y,
+                                 1, 1);
+            }
+        });
+    });
+}
+
+function playerDrop(){
+    player.pos.y++;
+    dropCounter = 0;
+}
+
+const player = {
+    pos: {x:5,y:5},
+    matrix:matrix,
+}
+
+function draw(){
+    context.fillStyle = '#000';
+    context.fillRect(0,0,canvas.width,canvas.height);
+    drawMatrix(player.matrix,player.pos);
+}
+
+document.addEventListener('keydown',event=>{
+    if(event.keyCode===37){
+        player.pos.x--;
+    }
+    else if(event.keyCode === 39){
+        player.pos.x++;
+    }
+    else if(event.keyCode === 40){
+        playerDrop(); 
+    }
+});
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update(time = 0){
+    const deltaTime = time- lastTime;
+    lastTime = time;
+
+    dropCounter += deltaTime;
+    if(dropCounter > dropInterval){
+        playerDrop();
+    }
+
+    draw();
+    requestAnimationFrame(update);
+}
+
+update();
+
+
+
